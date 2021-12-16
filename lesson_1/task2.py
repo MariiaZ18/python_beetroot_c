56d56b9571b07a71342a696cbc0bc70f1b07cab2
MariiaZ18 <95958931+MariiaZ18@users.noreply.github.com>
2021-12-16 14:58:49 +0100

    Delete lesson_1 directory

--- lesson_1/task2.py
+++
@@ -1,6 +0,0 @@
-print("Hello World!\n")
-# 'Hello World!'
-print('67,89,09', end='\n\n')
-print("My name is Mariia", end='\n')
-print("What is your name?\n")
-print(' ', 1, 2, 3, 4, 5, 6, 7, 8, 9, sep=' № ')

25a934508a3b484edcae86f82dc9c289ca423e68
MariiaZ18 <zejkanmaria5@gmail.com>
2021-12-17 12:23:19 +0100

    Task 2 of lesson 5. Manipulate strings.

---
+++ task_2.py
@@ -0,0 +1,7 @@
+# Manipulate strings.
+# Save your first and last name as separate variables, then use string concatenation to add them together with a white space in between and print a greeting.
+
+first_name = 'Mariia'
+last_name = 'Zeikan'
+
+print(f'{first_name}' + ' ' + f'{last_name}')

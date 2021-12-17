9be643aa8943c40adffd68b6b6007c6c17b69d83
MariiaZ18 <zejkanmaria5@gmail.com>
2021-12-17 12:44:15 +0100

    Task 3 of lesson 5.#Using python as a calculator.

---
+++ task_3.py
@@ -0,0 +1,21 @@
+# Using python as a calculator.
+# Make a program with 2 numbers saved in separate variables a and b, then print the result for each of the following:
+# Addition, subtraction, division, multiplication, exponent(Power), modulus, floor division
+
+a = 12
+b = 8
+
+# Addition
+print(f'{a} + {b} =', a + b)
+# subtraction
+print(f'{a} - {b} =', a - b)
+# division
+print(f'{a} / {b} =', a / b)
+# multiplication
+print(f'{a} * {b} =', a * b)
+# exponent(Power)
+print(f'{a} ** {b} =', a ** b)
+# modulus
+print(f'{a} % {b} =', a % b)
+# floor division
+print(f'{a} / {b} =', int(a / b))

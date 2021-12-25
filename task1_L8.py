f4cd864bb05c196dffb4cad0b84b0015db0a33f6
MariiaZ18 <zejkanmaria5@gmail.com>
2021-12-25 16:44:16 +0100

    Add task 1 of lesson 8.

---
+++ task1_L8.py
@@ -0,0 +1,18 @@
+# Write a function called oops that explicitly raises an IndexError exception when called.
+# Then write another function that calls oops inside a try/ except statement to catch the error.
+# What happens if you change oops to raise KeyError instead of IndexError?
+
+def oops():
+    my_list = ['Harry', 'Potter', 'and', 'the', 'Sorcerer\'s', 'Stone']
+    print(my_list[6])
+
+
+def main():
+    try:
+        oops()
+    except IndexError:
+        print('List index out of range')
+
+
+if __name__ == '__main__':
+    main()

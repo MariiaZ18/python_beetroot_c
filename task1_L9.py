056842dbee449dd84e9c40f194e5c3c9b3b64aba
MariiaZ18 <zejkanmaria5@gmail.com>
2021-12-27 22:10:22 +0100

    Add task 1 of lesson 9. The greatest number.

---
+++ task1_L9.py
@@ -0,0 +1,17 @@
+# The greatest number
+# # Write a Python program to get the largest number from a list of random numbers with the length of 10
+# Constraints: use only while loop and random module to generate numbers
+import random
+
+
+def greatest_number():
+    my_list = []
+    for i in range(0, 10):
+        i = random.randint(1, 2555)
+        my_list.append(i)
+    print(my_list)
+    print(max(my_list))
+
+
+if __name__ == '__main__':
+    greatest_number()
